498855116cecefa31ad880a25807a6f65e717a9d
LorrainBa <ordure.algebrique@gmail.com>
2024-01-09 11:17:37 +0100

    Suprresion fichier en trop

--- getScore.py
+++
@@ -1,9 +0,0 @@
-from loadFile  import load_comments
-from analyseSentiment import rate_global
-
-
-
-dataPath = "data/"
-file_name = "mcdo.txt"
-restaurant_comments = load_comments(file_name)
-rate_global(restaurant_comments)
