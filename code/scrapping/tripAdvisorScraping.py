68635a07b078f129afe5b61e183cb4d93d3c4367
Louis Floreani <100688035+Louis4933@users.noreply.github.com>
2024-01-10 10:45:42 +0100

    Update tripAdvisorScraping.py

--- code/scrapping/tripAdvisorScraping.py
+++ code/scrapping/tripAdvisorScraping.py
@@ -9,35 +9,65 @@ driver = webdriver.Firefox()
 #un exemple de page
 driver.get('https://www.tripadvisor.fr/Restaurant_Review-g187156-d3901925-Reviews-V_B-Perpignan_Pyrenees_Orientales_Occitanie.html')
 
+current_page = 1
+cookie_button_clicked = False
+
+#Boucle Tant que le bouton "Suivant" est clickable
 while True:
-    # Récupérer les données de la page
-    content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, "html.parser")
-
-    quotes = soup.find_all(class_="quote")
-    for quote in quotes:
-        print(quote.text)
-
-    comments = soup.find_all(class_="prw_rup prw_reviews_text_summary_hsx") 
-    for comment in comments:
-        print(comment.text)
-
-    # Essayer de cliquer sur le bouton "suivant"
+    
+    # Gérer la fenêtre contextuelle des cookies s'il y en a une
+    if not cookie_button_clicked:
+        
+        try:
+            cookie_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[text()="Tout refuser"]'))
+            )
+            cookie_button.click()
+            cookie_button_clicked = True
+            
+        except Exception as e:
+            print("Pas de fenêtre contextuelle des cookies ou impossible de la traiter.")
+        
+    print(current_page)
+    
+    #On essaie de cliquer sur le bouton "suivant"
     try:
         next_page_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.LINK_TEXT, 'Suivant'))
         )
         next_page_button.click()
+        
+        #On récupère le contenu des avis
+        content = driver.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(content, "html.parser")
 
-        # Attendre que la page se charge
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "quote"))
-        )
-
+        quotes = soup.find_all(class_="noQuotes")
+        comments = soup.find_all(class_="prw_rup prw_reviews_text_summary_hsx")
+        
+        for quote in quotes:
+            print(quote.text)
+            
+        for comment in comments:
+            print(comment.text)
+            
+        current_page = current_page + 1
+        
     except Exception as e:
-        # Le bouton "suivant" n'est plus clickable, sortir de la boucle
-        print("Fin de la pagination.")
+        # Le bouton "Suivant" n'est plus clickable, sortir de la boucle
         break
+        
+# Récupérer les données de la dernière page
+content = driver.page_source.encode('utf-8').strip()
+soup = BeautifulSoup(content, "html.parser")
+
+quotes = soup.find_all(class_="noQuotes")
+comments = soup.find_all(class_="prw_rup prw_reviews_text_summary_hsx")
+
+for quote in quotes:
+    print(quote.text)
+
+for comment in comments:
+    print(comment.text)
 
 # Fermer le navigateur à la fin
 driver.quit()
